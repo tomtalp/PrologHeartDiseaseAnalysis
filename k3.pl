10e0c510b26cb946b68465e9f1415eb1bd0897ee
tomtalp <tomtalp93@gmail.com>
2019-10-12 16:26:29 +0300

    WIP

--- k3.pl
+++ k3.pl
@@ -1,4 +1,17 @@
+:- op(800, fx, if).
+:- op(700, xfx, then).
+:- op(300, xfy, or).
+:- op(200, xfy, and).
+
 :- dynamic(already_asked/1).
+:- dynamic(knowledge/2).
+
+askable(blood_sugar).
+askable(resting_bp).
+askable(max_exer_difficulty).
+if knowledge(blood_sugar, high) and knowledge(resting_bp, high) then knowledge(heart_risk, true).
+
+%is_true(knowledge(heart_risk, true)
 
 start:-
     write('Welcome to the Heart Disease Analysis expert system!'),nl,
@@ -15,7 +28,8 @@ start:-
     Action == quit.
 
 do(diagnose):-
-    !, write('Performing diagnose!'), nl.    
+    !, write('Performing diagnose!'), 
+    check_heart_risk.    
 
 do(help):-
     !, write("Here's some info about our system - blablabla") ,nl.    
@@ -26,16 +40,50 @@ do(Unknown):-
     write("Received an illegal operation `"), write(Unknown), write("`, please try again (type `help` for the manual)"),nl.
 
 
+check_heart_risk:-
+    is_true(knowledge(heart_risk, true)),!,
+    write('The patient is in heart risk.'),nl.
+
+check_heart_risk:-
+    write('NO HEART RISK!'),nl.
+
 % What is the maximum difficulty on the ECG treadmil test?  (easy/medium/hard)
 max_exer_difficulty(X):-
     ask_user(max_exer_difficulty, X, [easy, medium, hard]).
 
+human_readable_question(cp_only_exercise):-
+    write('Is there chest pain only during exercise? (yes/no) : '), nl, !.
+
+human_readable_question(max_exer_difficulty):-
+    write('What is the maximum difficulty reached on the ECG treadmil test?  (easy/medium/hard) '), nl, !.    
+
+human_readable_question(cholesterol_level):-
+    write('What is the patients cholesterol level?  (low/normal/high) '), nl, !.   
+
+human_readable_question(blood_sugar):-
+    write('What is the patients blood sugar level?  (low/high) '), nl, !. 
+
+human_readable_question(resting_bp):-
+    write('What is the patients resting blood pressure?  (low/normal/high) '), nl, !.   
+
+get_multianswer_options(cp_only_exercise, [yes, no]).
+
+get_multianswer_options(max_exer_difficulty, [easy, medium, hard]).
+
+get_multianswer_options(cholesterol_level, [low, normal, high]).
+
+get_multianswer_options(blood_sugar, [low, high]).
+
+get_multianswer_options(resting_bp, [low, normal, high]).
+
 ask_user(Attr, Val, Options):-
-    write("What's the value for `"), write(Attr), write('`? '),nl,
-    write(Options), write(' : '), nl,
+    human_readable_question(Attr),
     read(UserAnswer),
     validate_answer(UserAnswer, Attr, Options),!,
-    asserta(already_asked(Attr)).
+    asserta(already_asked(Attr)), % Make sure we remember we asked
+    Val = UserAnswer,
+    asserta(knowledge(Attr, Val)). % Save the value
+
 
 % Received a valid answer
 validate_answer(UserAnswer, Attr, Options):-
@@ -46,5 +94,61 @@ validate_answer(UserAnswer, Attr, Options):-
     write(UserAnswer), write(" is an invalid answer. Please try again."),nl,
     ask_user(Attr, Val, Options).
 
+is_true(knowledge(Attr1, Val1)):-
+    explore(knowledge(Attr1, Val1)).
+
+explore(knowledge(Attr1, Val1)):- 
+    write("Evaluating "), write(Attr1), write(" in regular exploration"),nl,
+    knowledge(Attr1, Val1), !.
+
+explore(knowledge(Attr1, Val1)):- 
+    write("Evaluating "), write(Attr1), write(" in askable exploration"),nl,
+    askable(Attr1), 
+    \+ knowledge(Attr1, _),
+    \+ already_asked(Attr1),
+    get_multianswer_options(Attr1, Options),
+    ask_user(Attr1, Val, Options),
+    Val == Val1.
 
 
+explore(knowledge(Attr1, Val1) and knowledge(Attr2, Val2)):-
+    %s!, % DO WE REALLY NEED THIS?? % If we match with an `and` clause, no need to check other types of clauses
+    write("Evaluating "), write(Attr1), write(" in AND exploration"),nl,
+    explore(knowledge(Attr1, Val1)),
+    explore(knowledge(Attr2, Val2)),!.
+
+explore(knowledge(Attr1, Val1) or knowledge(Attr2, Val2)):-
+    (
+        explore(knowledge(Attr1, Val1))
+        ;
+        explore(knowledge(Attr2, Val2))
+    ), !.    
+
+
+
+explore(P):-
+    if Cond then P,
+    explore(Cond).
+
+/*
+is_true(P1 and P2):-
+    !, % If we match with an `and` clause, no need to check other types of clauses
+    is_true(P1),
+    is_true(P2).
+
+is_true(P1 or P2):-
+    (
+        is_true(P1)
+        ;
+        is_true(P2)
+    ), !.    
+
+is_true(P):- 
+    P, !.    
+
+is_true(P):-
+    if Cond then P,
+    is_true(Cond).
+
+is_true(P)    
+*/
