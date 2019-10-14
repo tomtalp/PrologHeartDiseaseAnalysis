93510495161b6fb79e13955dc2a269efcbcc3b94
tomtalp <tomtalp93@gmail.com>
2019-10-14 09:59:37 +0300

    finished if/else rules

--- rules.pl
+++ rules.pl
@@ -46,3 +46,146 @@ if
     and knowledge(resting_bp, high)
 then 
     knowledge(heart_risk, true).        
+
+if 
+    knowledge(cp_only_exercise, yes) 
+    and knowledge(max_exer_difficulty, medium) 
+    and (knowledge(cholesterol_level, low) or knowledge(cholesterol_level, normal))
+then 
+    knowledge(heart_risk, false).        
+
+if 
+    knowledge(cp_only_exercise, yes) 
+    and knowledge(max_exer_difficulty, medium) 
+    and knowledge(cholesterol_level, high)
+then 
+    knowledge(heart_risk, true).            
+
+if 
+    knowledge(cp_only_exercise, yes) 
+    and knowledge(max_exer_difficulty, hard) 
+then 
+    knowledge(heart_risk, false).                
+
+if 
+    knowledge(cp_only_exercise, no) 
+    and knowledge(max_exer_difficulty, easy) 
+    and knowledge(gender, female)
+then 
+    knowledge(heart_risk, true).       
+
+if 
+    knowledge(cp_only_exercise, no) 
+    and knowledge(max_exer_difficulty, easy) 
+    and knowledge(gender, male)
+    and knowledge(blood_sugar, high)
+then 
+    knowledge(heart_risk, true).     
+
+if 
+    knowledge(cp_only_exercise, no) 
+    and knowledge(max_exer_difficulty, easy) 
+    and knowledge(gender, male)
+    and knowledge(blood_sugar, low)
+    and knowledge(resting_bp, high)
+then 
+    knowledge(heart_risk, true). 
+
+if 
+    knowledge(cp_only_exercise, no) 
+    and knowledge(max_exer_difficulty, easy) 
+    and knowledge(gender, male)
+    and knowledge(blood_sugar, low)
+    and knowledge(resting_bp, normal)
+    and (knowledge(cholesterol_level, normal) or knowledge(cholesterol_level, high))
+then 
+    knowledge(heart_risk, true).    
+
+if 
+    knowledge(cp_only_exercise, no) 
+    and knowledge(max_exer_difficulty, easy) 
+    and knowledge(gender, male)
+    and knowledge(blood_sugar, low)
+    and knowledge(resting_bp, normal)
+    and knowledge(cholesterol_level, low)
+then 
+    knowledge(heart_risk, false).  
+
+if 
+    knowledge(cp_only_exercise, no) 
+    and knowledge(max_exer_difficulty, easy) 
+    and knowledge(gender, male)
+    and knowledge(blood_sugar, low)
+    and knowledge(resting_bp, low)
+    and (knowledge(cholesterol_level, low) or knowledge(cholesterol_level, normal))
+then 
+    knowledge(heart_risk, false).       
+
+if 
+    knowledge(cp_only_exercise, no) 
+    and knowledge(max_exer_difficulty, easy) 
+    and knowledge(gender, male)
+    and knowledge(blood_sugar, low)
+    and knowledge(resting_bp, low)
+    and knowledge(cholesterol_level, high)
+then 
+    knowledge(heart_risk, true).   
+
+if 
+    knowledge(cp_only_exercise, no) 
+    and knowledge(max_exer_difficulty, medium) 
+    and knowledge(gender, male)
+    and knowledge(resting_bp, low)
+then 
+    knowledge(heart_risk, false).  
+
+if 
+    knowledge(cp_only_exercise, no) 
+    and knowledge(max_exer_difficulty, medium) 
+    and knowledge(gender, male)
+    and knowledge(resting_bp, normal)
+    and (knowledge(cholesterol_level, low) or knowledge(cholesterol_level, normal))
+then 
+    knowledge(heart_risk, false).      
+
+if 
+    knowledge(cp_only_exercise, no) 
+    and knowledge(max_exer_difficulty, medium) 
+    and knowledge(gender, male)
+    and knowledge(resting_bp, normal)
+    and knowledge(cholesterol_level, high)
+then 
+    knowledge(heart_risk, true). 
+
+if 
+    knowledge(cp_only_exercise, no) 
+    and knowledge(max_exer_difficulty, medium) 
+    and knowledge(gender, male)
+    and knowledge(resting_bp, high)
+    and knowledge(cholesterol_level, low)
+then 
+    knowledge(heart_risk, false).  
+
+if 
+    knowledge(cp_only_exercise, no) 
+    and knowledge(max_exer_difficulty, medium) 
+    and knowledge(gender, male)
+    and knowledge(resting_bp, high)
+    and (knowledge(cholesterol_level, normal) or knowledge(cholesterol_level, high))
+then 
+    knowledge(heart_risk, true).                                        
+
+if 
+    knowledge(cp_only_exercise, no) 
+    and knowledge(max_exer_difficulty, hard) 
+    and (knowledge(cholesterol_level, low) or knowledge(cholesterol_level, normal))
+then 
+    knowledge(heart_risk, false).  
+
+if 
+    knowledge(cp_only_exercise, no) 
+    and knowledge(max_exer_difficulty, hard) 
+    and knowledge(cholesterol_level, high)
+then 
+    knowledge(heart_risk, true).      
+    
